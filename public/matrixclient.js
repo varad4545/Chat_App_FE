55ca9658e3470b14d4f571e8cc25a253fbef1308
varad4545 <68903734+varad4545@users.noreply.github.com>
2022-06-30 17:50:25 +0530

    Delete public directory

--- public/matrixclient.js
+++
@@ -1,9 +0,0 @@
-import {createClient} from '../node_modules/matrix-js-sdk/lib/matrix.js'
-
-const matrixclient=createClient({
-    baseUrl:"https://matrix.org",
-    accessToken: process.env.accessToken,
-    userId: process.env.username
-})
-
-export default matrixclient
